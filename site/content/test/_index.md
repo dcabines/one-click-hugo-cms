cf6fce9011c618cc4b5c00a80d7714881fac2b78
David Cabiness <dcabines@gmail.com>
2020-05-01 20:32:22 -0400

    messed around

---
+++ site/content/test/_index.md
@@ -0,0 +1,9 @@
+---
+title: "TEST"
+image: /img/about-jumbotron.jpg
+values:
+  - heading: "HEADING"
+    text: >
+      CONTENT
+    imageUrl: "/img/about-shade-grown.jpg"
+---
---
+++ src/js/test.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { List } from 'immutable';
+
+import Jumbotron from "./components/jumbotron";
+
+const MediaBlock = ({heading, text, imageUrl, reverse}) => {
+  const imageContainerClassName = reverse
+    ? "ph3-m w-50-m"
+    : "ph3-m w-50-m order-last-m";
+  return <div className="flex-m mhn3-m mb4">
+    <div className={imageContainerClassName}>
+      <img src={imageUrl} alt="" className="db mb2" />
+    </div>
+    <div className="ph3-m w-50-m">
+      <h3 className="f3 b lh-title mb1">{heading}</h3>
+      <p>{text}</p>
+    </div>
+  </div>;
+};
+
+export default class ValuesPreview extends React.Component {
+  render() {
+    const {entry, getAsset} = this.props;
+    
+    let image = getAsset(entry.getIn(["data", "image"]));
+
+   
+    const entryValues = entry.getIn(["data", "values"]);
+    const values = entryValues ? entryValues.toJS() : [];
+    
+    return <div>
+      CONTENT
+    </div>;
+  }
+}
